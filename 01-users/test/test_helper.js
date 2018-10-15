522b754a3509261a6ed50b2c54c832fe9cf33752
Sarath <sarath.hacks@gmail.com>
2018-10-15 15:14:18 +0530

    :bug: Fixed clearing connection before each test

--- 01-users/test/test_helper.js
+++ 01-users/test/test_helper.js
@@ -1,52 +1,29 @@
 const mongoose = require('mongoose')
+const User = require('../src/user')
+const BlogPost = require('../src/blogPost')
+const Comment = require('../src/comment')
 
 // MongoDB Connection 😄
 
-const mongoDBUrl = 'mongodb://localhost:27017/mongo-for-monks';
+const mongoDBUrl = 'mongodb://localhost:27017/mongo-for-monks'
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
-before((done) => {
-    
-    mongoose.connect(mongoDBUrl,{ useNewUrlParser: true})
-    mongoose.connection.once('open', ()=> {
-        done();
+before(done => {
+  mongoose.connect(mongoDBUrl, { useNewUrlParser: true })
+  mongoose.connection
+    .once('open', () => {
+      done()
     })
-    .on('error', (error)=> {
-        console.warn('WARNING: '. error)
+    .on('error', error => {
+      console.warn('WARNING: '.error)
     })
 })
-
-
 
 // Before each test
 
-beforeEach((done) => {
-
-
-    // Drop user collections
-    mongoose.connection.collections.users.drop()
-    .then(() => {
-
-        mongoose.connection.collections.blogposts.drop()
-        .then(() => {
-
-            mongoose.connection.collections.comments.drop()
-                .then(() => {
-
-                    done();
-                })
-                .catch(() => { console.log('>>> Error'); })
-
-        })
-        .catch((e) => {console.log('>>Error', e); })
-    })
-    .catch((e) => {console.log('> Error ', e); done() })
-
-
-
-
+beforeEach(async function () {
+  await User.deleteMany()
+  await BlogPost.deleteMany()
+  await Comment.deleteMany()
 })
-
-
-
