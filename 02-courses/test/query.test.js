cca53241c3b065d17c5ecc0d04d307612e146804
Sarath <sarath.hacks@gmail.com>
2018-10-22 19:23:52 +0530

    Comparison query and operators

---
+++ 02-courses/test/query.test.js
@@ -0,0 +1,105 @@
+const assert = require('assert')
+const Course = require('../src/models/Course')
+
+describe('Query docs', () => {
+  beforeEach(async function () {
+    const docs = [
+      {
+        name: 'React JS Course',
+        author: 'Wes Bos',
+        price: 25,
+        tags: ['JavaScript', 'React'],
+        isPublished: true
+      },
+      {
+        name: 'Node JS Course',
+        author: 'Mosh',
+        price: 20,
+        tags: ['JavaScript', 'Node'],
+        isPublished: false
+      },
+      {
+        name: 'Angular Crash Course',
+        author: 'Brad',
+        price: 10,
+        tags: ['JavaScript', 'Angular'],
+        isPublished: true
+      },
+      {
+        name: 'Laravel',
+        author: 'Jeffrey Way',
+        price: 33,
+        tags: ['php', 'Laravel'],
+        isPublished: true
+      }
+    ]
+
+    await Course.create(docs)
+  })
+
+  it('Find published courses', async function () {
+    const courses = await Course.find({ isPublished: true })
+
+    assert.equal(courses[1].isPublished, true)
+
+    assert.equal(courses.length, 3)
+  })
+
+  it('Sort by name and get the first two docs', async function () {
+    const courses = await Course.find({}).limit(2).sort({ name: 1 })
+    assert.equal(courses[0].name, 'Angular Crash Course')
+    assert.equal(courses[1].name, 'Laravel')
+    assert.equal(courses.length, 2)
+  })
+
+  it('Sort name descending  , get last 2 docs', async function () {
+    const courses = await Course.find({}).sort({ name: -1 }).skip(2).limit(2)
+    assert.equal(courses[1].name, 'Angular Crash Course')
+    assert.equal(courses[0].name, 'Laravel')
+    assert.equal(courses.length, 2)
+  })
+
+  it('Select only sorted course names ', async function () {
+    const courses = await Course.find({})
+      .sort({ name: 1 })
+      .select({ name: 1, _id: 0 })
+    assert.equal(courses[0]._id, undefined)
+    assert.equal(courses[0].name, 'Angular Crash Course')
+  })
+
+  // Comparison operators
+
+  /**
+   * $gt - greater than
+   * $gte - greater than eqaul to
+   */
+
+  it('Get courses where price greater than 15', async function () {
+    const courses = await Course.find({ price: { $gt: 15 } })
+      .select('name price -_id')
+      .sort({ price: 1 })
+    assert.equal(courses[0].price, 20)
+    assert.equal(courses[2].price, 33)
+  })
+  it('Get courses where price greater than or equal to 25', async function () {
+    const courses = await Course.find({ price: { $gte: 25 } })
+      .select('name price -_id')
+      .sort({ price: 1 })
+    assert.equal(courses[0].price, 25)
+    assert.equal(courses[1].price, 33)
+  })
+
+  it('Select courser where taged in JavaScript', async function () {
+    const courses = await Course.find({ tags: { $in: 'JavaScript' } })
+
+    assert.equal(courses.length, 3)
+  })
+
+  it('Select courser where taged in React Or Node', async function () {
+    const courses = await Course.find({
+      tags: { $in: ['Node', 'React'] }
+    })
+
+    assert.equal(courses.length, 2)
+  })
+})
